f09b15c6a998d0ad878b0576433dd1736febcad6
mmmarciii <mesz.marc@gmail.com>
2025-11-16 14:37:19 +0100

    Hamburger menu fix

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -8,18 +8,17 @@ function Navbar() {
   return (
     <nav className={styles.container}>
         <div className={styles.navbarCenter}>
-            <nav>
                 <div className={styles.navMenu}>
                     <NavLinks />
+                </div>
+                <div className={styles.hamburgerIcon}>
+                    <Hamburger rounded easing="ease-in" toggled={isDisplayed} toggle={setDisplayed}/>
                 </div>
                 <div className={styles.hamburgerMenu}>
                         {isDisplayed && <NavLinks />}
                 </div>
-            </nav>
         </div>
-        <div className={styles.hamburgerIcon}>
-            <Hamburger rounded easing="ease-in" toggled={isDisplayed} toggle={setDisplayed}/>
-        </div>
+
     </nav>
   )
 }
